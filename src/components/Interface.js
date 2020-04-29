804cd5da05bdc9415f87022b99aae05fee64dcf5
ejilee <whyejilee@gmail.com>
2020-04-29 16:58:45 +0900

    started adding material-ui interface

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,98 +1,89 @@
 import React, { useState, useCallback } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
 
-const Interface = ({ rotPer, orbPer, orbRad, mooSiz, plaSiz, sunDir, setRotOrbPer, setOrbRad, setBodySize, setSunDirecttion }) => {
-  const [form, setForm] = useState({
-    rotationalPeriod: rotPer,
-    orbitalPeriod: orbPer,
-    orbitRadius: orbRad,
-    moonSize: mooSiz,
-    planetSize: plaSiz,
-    sunDirection: sunDir,
-  });
+const Interface = ({
+    rotPer,
+    orbPer,
+    orbRad,
+    mooSiz,
+    plaSiz,
+    sunDir,
+    setRotOrbPer,
+    setOrbRad,
+    setBodySize,
+    setSunDirecttion
+}) => {
+    const [form, setForm] = useState({
+        rotationalPeriod: rotPer,
+        orbitalPeriod: orbPer,
+        orbitRadius: orbRad,
+        moonSize: mooSiz,
+        planetSize: plaSiz,
+        sunDirection: sunDir
+    });
 
-  const changeInputValue = useCallback(
-    e => {
-      const nextForm = {
-        ...form,
-        [e.target.name]: e.target.value
-      };
-      setForm(nextForm);
-    },
-    [form]
-  );
+    const changeInputValue = useCallback(
+        e => {
+            const nextForm = {
+                ...form,
+                [e.target.name]: e.target.value
+            };
+            setForm(nextForm);
+        },
+        [form]
+    );
 
-  const onSubmit = useCallback(
-    e => {
-      setRotOrbPer(form.rotationalPeriod, form.orbitalPeriod);
-      setOrbRad(form.orbitRadius);
-      setBodySize(form.moonSize, form.planetSize);
-      setSunDirecttion(form.sunDirection);
-      e.preventDefault();
-    },
-    [setRotOrbPer,setOrbRad,setBodySize, setSunDirecttion, form]
-  );
+    const onSubmit = useCallback(
+        e => {
+            setRotOrbPer(form.rotationalPeriod, form.orbitalPeriod);
+            setOrbRad(form.orbitRadius);
+            setBodySize(form.moonSize, form.planetSize);
+            setSunDirecttion(form.sunDirection);
+            e.preventDefault();
+        },
+        [setRotOrbPer, setOrbRad, setBodySize, setSunDirecttion, form]
+    );
 
-  return (
-    <div className="uiBox">
-      <form onSubmit={onSubmit}>
-        Orbit radius :<br />
-        <input
-          type="number"
-          name="orbitRadius"
-          value={form.orbitRadius}
-          onChange={changeInputValue}
-        />
-        <br />
-        <br />
-        Rotational period :<br />
-        <input
-          type="number"
-          name="rotationalPeriod"
-          value={form.rotationalPeriod}
-          onChange={changeInputValue}
-        />
-        <br />
-        <br />
-        Orbital period :<br />
-        <input
-          type="number"
-          name="orbitalPeriod"
-          value={form.orbitalPeriod}
-          onChange={changeInputValue}
-        />
-        <br />
-        <br />
-        Earth size :<br />
-        <input
-          type="number"
-          name="planetSize"
-          value={form.planetSize}
-          onChange={changeInputValue}
-        />
-        <br />
-        <br />
-        Moon size :<br />
-        <input
-          type="number"
-          name="moonSize"
-          value={form.moonSize}
-          onChange={changeInputValue}
-        />
-        <br />
-        <br />
-        Direction of Light :<br />
-        <input
-          type="number"
-          name="sunDirection"
-          value={form.sunDirection}
-          onChange={changeInputValue}
-        />
-        <br />
-        <br />
-        <button type="submit">SET</button>
-      </form>
-    </div>
-  );
+    return (
+        <Paper className="uiBox">
+            <form onSubmit={onSubmit}>
+                <TextField
+                    id="rotationalPeriod"
+                    name="rotationalPeriod"
+                    label="Rotational Period"
+                    type="number"
+                    variant="outlined"
+                    hintText="the time it takes for the moon to complete a rotation on its own axis"
+                    floatingLabelText="Rotational Period"
+                    onChange={changeInputValue}
+                    defaultValue={form.rotationalPeriod}
+                    color="inherit"
+                />
+                <TextField
+                    id="orbitalPeriod"
+                    name="orbitalPeriod"
+                    label="Orbital Period"
+                    type="number"
+                    variant="outlined"
+                    hintText="the time it takes for the moon to complete a revolution around the bigger body"
+                    floatingLabelText="Orbital Period"
+                    onChange={changeInputValue}
+                    defaultValue={form.orbitalPeriod}
+                    color="inherit"
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    color="secondary"
+                >
+                    SET
+                </Button>
+            </form>
+        </Paper>
+    );
 };
 
 export default Interface;
--- src/components/Space.js
+++ src/components/Space.js
@@ -8,86 +8,81 @@ import "./Space.scss";
 const initialSetting = {
     rotationalPeriod: 30,
     orbitalPeriod: 30,
-    orbitRadius: 200,
+    orbitRadius: 100,
     moonSize: 30,
     planetSize: 90,
     sunDirection: 100
 };
 
-function settingReducer(setting, action){
-  switch (action.type){
-    case 'ROTATIONAL_PERIOD':
-      return {...setting, rotationalPeriod : action.rotPer};
-    case 'ORBITAL_PERIOD':
-      return {...setting, orbitalPeriod : action.orbPer};
-    case 'ORBIT_RADIUS':
-      return {...setting, orbitRadius : action.orbRad};
-    case 'MOON_SIZE':
-      return {...setting, moonSize : action.mooSiz};
-    case 'PLANET_SIZE':
-        return {...setting, planetSize : action.plaSiz};
-    case 'SUN_DIRECTION':
-        return {...setting, sunDirection : action.sunDir};
-    default :
-      return setting;
-  }
+function settingReducer(setting, action) {
+    switch (action.type) {
+        case "ROTATIONAL_PERIOD":
+            return { ...setting, rotationalPeriod: action.rotPer };
+        case "ORBITAL_PERIOD":
+            return { ...setting, orbitalPeriod: action.orbPer };
+        case "ORBIT_RADIUS":
+            return { ...setting, orbitRadius: action.orbRad };
+        case "MOON_SIZE":
+            return { ...setting, moonSize: action.mooSiz };
+        case "PLANET_SIZE":
+            return { ...setting, planetSize: action.plaSiz };
+        case "SUN_DIRECTION":
+            return { ...setting, sunDirection: action.sunDir };
+        default:
+            return setting;
+    }
 }
 
 const Space = () => {
+    const [setting, dispatchSetting] = useReducer(
+        settingReducer,
+        initialSetting
+    );
 
-  const [setting, dispatchSetting] = useReducer(settingReducer, initialSetting);
+    const setRotOrbPer = useCallback((rotPer, orbPer) => {
+        dispatchSetting({ type: "ROTATIONAL_PERIOD", rotPer });
+        dispatchSetting({ type: "ORBITAL_PERIOD", orbPer });
+    }, []);
 
-  const setRotOrbPer = useCallback((rotPer, orbPer) =>{
-    dispatchSetting({type: 'ROTATIONAL_PERIOD', rotPer});
-    dispatchSetting({type: 'ORBITAL_PERIOD', orbPer});
-  }, []
-  );
+    const setOrbRad = useCallback(orbRad => {
+        dispatchSetting({ type: "ORBIT_RADIUS", orbRad });
+        console.log(orbRad);
+    }, []);
 
-  const setOrbRad = useCallback((orbRad) =>{
-    dispatchSetting({type: 'ORBIT_RADIUS', orbRad});
-    console.log(orbRad);
-  }, []
-  );
+    const setBodySize = useCallback((mooSiz, plaSiz) => {
+        dispatchSetting({ type: "MOON_SIZE", mooSiz });
+        dispatchSetting({ type: "PLANET_SIZE", plaSiz });
+    }, []);
 
-  const setBodySize = useCallback((mooSiz, plaSiz) => {
-    dispatchSetting({type: 'MOON_SIZE', mooSiz});
-    dispatchSetting({type: 'PLANET_SIZE', plaSiz});
-  },[]);
+    const setSunDirecttion = useCallback(sunDir => {
+        dispatchSetting({ type: "SUN_DIRECTION", sunDir });
+    }, []);
 
-  const setSunDirecttion = useCallback((sunDir) => {
-    dispatchSetting({type: 'SUN_DIRECTION', sunDir});
-  },[]);
-
-
-  return (
-    <div className="spaceContainer">
-      <Planet
-        plaSiz={setting.planetSize}
-        sunDir={setting.sunDirection}
-      />
-      <OrbitLine
-        orbRad={setting.orbitRadius} />
-      <Moon 
-        rotPer={setting.rotationalPeriod}
-        orbPer={setting.orbitalPeriod}
-        orbRad={setting.orbitRadius}
-        mooSiz={setting.moonSize}
-        sunDir={setting.sunDirection}
-      />
-      <Interface
-        rotPer={setting.rotationalPeriod}
-        orbPer={setting.orbitalPeriod}
-        orbRad={setting.orbitRadius}
-        mooSiz={setting.moonSize}
-        plaSiz={setting.planetSize}
-        sunDir={setting.sunDirection}
-        setRotOrbPer={setRotOrbPer}
-        setOrbRad={setOrbRad}
-        setBodySize={setBodySize}
-        setSunDirecttion={setSunDirecttion}
-      />
-    </div>
-  );
+    return (
+        <div className="spaceContainer">
+            <Planet plaSiz={setting.planetSize} sunDir={setting.sunDirection} />
+            <OrbitLine orbRad={setting.orbitRadius} />
+            <Moon
+                rotPer={setting.rotationalPeriod}
+                orbPer={setting.orbitalPeriod}
+                orbRad={setting.orbitRadius}
+                mooSiz={setting.moonSize}
+                sunDir={setting.sunDirection}
+            />
+            <Interface
+                rotPer={setting.rotationalPeriod}
+                orbPer={setting.orbitalPeriod}
+                orbRad={setting.orbitRadius}
+                mooSiz={setting.moonSize}
+                plaSiz={setting.planetSize}
+                sunDir={setting.sunDirection}
+                setRotOrbPer={setRotOrbPer}
+                setOrbRad={setOrbRad}
+                setBodySize={setBodySize}
+                setSunDirecttion={setSunDirecttion}
+            />
+        </div>
+    );
 };
 
 export default Space;
