416598eea7a74c8694e3bd1adba7417618ec60a5
ejilee <whyejilee@gmail.com>
2020-05-27 10:14:53 +0900

    added link to website on logo image

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,7 +4,9 @@ import logo from "./logo.svg";
 const Header = () => {
   return (
     <header className="app-header">
-      <img src={logo} className="logoImg" alt="logo" />
+      <a className="logo" href="https://tidallylocked.com" target="_self">
+        <img src={logo} className="logoImg" alt="logo" />
+      </a>
       <h1 className="titleText">Tidally Locked</h1>
     </header>
   );
